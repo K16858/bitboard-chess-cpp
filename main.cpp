975ca71ae0063519ed722a09a33e13cc3e2dc6e9
k16858 <ktohda0127@gmail.com>
2026-01-17 22:27:21 +0900

    ボード表示機能を追加

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ class Bitboard {
     public:
         Bitboard(U64 board);
         U64 GetBoard() const;
+        void PrintBoard() const;
     private:
         U64 board;
 };
@@ -19,8 +20,22 @@ U64 Bitboard::GetBoard() const {
     return board;
 }
 
+void Bitboard::PrintBoard() const {
+    std::cout << std::endl;
+    for (int rank = 7; rank >= 0; rank--) {
+        std::cout << rank + 1 << " ";
+        for (int file = 0; file < 8; file++) {
+            int square = rank * 8 + file;
+            U64 bit = 1ULL << square;
+            std::cout << (board & bit ? "1 " : ". ");
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "  a b c d e f g h" << std::endl << std::endl;
+}
+
 int main() {
-    Bitboard bb(0x0000000000000000);
-    std::cout << bb.GetBoard() << std::endl;
+    Bitboard bb(0x000000000000FF00);
+    bb.PrintBoard();
     return 0;
 }
