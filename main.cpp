68da96ca8f2b798aa8539a5275e159355652aa26
k16858 <ktohda0127@gmail.com>
2026-01-17 22:23:23 +0900

    ビットボードクラスを追加

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstdint>
+
+typedef uint64_t U64;
+
+class Bitboard {
+    public:
+        Bitboard(U64 board);
+        U64 GetBoard() const;
+    private:
+        U64 board;
+};
+
+Bitboard::Bitboard(U64 board) {
+    this->board = board;
+}
+
+U64 Bitboard::GetBoard() const {
+    return board;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Bitboard bb(0x0000000000000000);
+    std::cout << bb.GetBoard() << std::endl;
     return 0;
 }
