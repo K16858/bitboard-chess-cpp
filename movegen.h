e30ae49f1f4068e2d48b0dca04ef8a6840b81eeb
k16858 <ktohda0127@gmail.com>
2026-01-18 11:36:41 +0900

    移動生成の宣言を追加

--- movegen.h
+++ movegen.h
@@ -6,19 +6,31 @@
 
 class MoveGen {
 private:
-    static U64 kingMoves[64];
+    static U64 pawnMoves[64];
+    static U64 rookMoves[64];
+    static U64 bishopMoves[64];
     static U64 knightMoves[64];
+    static U64 queenMoves[64];
+    static U64 kingMoves[64];
     static bool initialized;
     
-    static void InitKingMoves();
+    static void InitPawnMoves();
+    static void InitRookMoves();
+    static void InitBishopMoves();
     static void InitKnightMoves();
-    
+    static void InitQueenMoves();
+    static void InitKingMoves();
+
     static U64 GenerateMoves(int square, const int offsets[], int numOffsets, std::function<bool(int, int)> isValidMove);
     
 public:
-    static void Init();
-    static U64 GetKingMoves(Square square);
+    static void Init(); 
+    static U64 GetPawnMoves(Square square);
+    static U64 GetRookMoves(Square square);
+    static U64 GetBishopMoves(Square square);
     static U64 GetKnightMoves(Square square);
+    static U64 GetQueenMoves(Square square);
+    static U64 GetKingMoves(Square square);
 };
 
 #endif
