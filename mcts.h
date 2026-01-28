b365a93516c22161995427092e3817ec0c63d64a
k16858 <ktohda0127@gmail.com>
2026-01-28 12:06:30 +0900

    MCTSのヘッダファイルを追加

---
+++ mcts.h
@@ -0,0 +1,27 @@
+#ifndef MCTS_H
+#define MCTS_H
+
+#include "board.h"
+#include "move.h"
+#include <random>
+#include <utility>
+#include <vector>
+
+struct MCTSNode {
+    Move move_from_parent;
+    MCTSNode* parent;
+    std::vector<MCTSNode*> children;
+    int N;
+    double W;
+};
+
+// RunMCTSの戻り値
+struct MCTSResult {
+    std::vector<std::pair<Move, int>> visits;
+    double rootValue;
+    int rootVisits;
+};
+
+MCTSResult RunMCTS(const Board& root, int iterations, std::mt19937& gen);
+
+#endif
